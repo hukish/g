faf3f0a0bacedc7391d5280c0f7d745c2852f899
hukish <hudsonhukish@gmail.com>
2020-05-12 12:09:11 +0300

    modify changes

--- app/models.py
+++ app/models.py
@@ -19,3 +19,6 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.username}'
