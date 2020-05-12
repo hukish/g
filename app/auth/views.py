56ba01ae5dff31113aabd18b1ea68e7d130854d6
hukish <hudsonhukish@gmail.com>
2020-05-12 15:16:53 +0300

    modify changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,26 +1,10 @@
-from flask import render_template,redirect,url_for,flash
-from flask_login import login_user,logout_user,login_required
-from ..models import User
-from .forms import LoginForm,RegistrationForm
-from .. import db
 from . import auth
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
-
-
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
+from flask import render_template,redirect,url_for,flash,request
+from ..models import User
+from flask_login import login_user,logout_user,login_required
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -33,12 +17,26 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "events login"
+    title = "event login"
     return render_template('auth/login.html',login_form = login_form,title=title)
-
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        #mail_message("Welcome ","email/welcome_user",user.email,user=user)
+         
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
