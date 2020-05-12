fdd0e7bb7f42736dcb27e294b45cf9ef397212cf
hukish <hudsonhukish@gmail.com>
2020-05-12 12:08:30 +0300

    created the user and name column in the role class

--- app/models.py
+++ app/models.py
@@ -17,3 +17,5 @@ class Role(db.Model):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
